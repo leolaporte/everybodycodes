13c3eaec0270ca08e4b0f8f89043308ca7e22832
Leo Laporte <recovery@yepmail.net>
2025-11-07 10:50:57 -0800

    2025 Quest 2 done

--- 2025/quest02/q02.lisp
+++ 2025/quest02/q02.lisp
@@ -2,7 +2,7 @@
 ;;;; 2025 Everybody Codes solution
 ;;;; Leo Laporte
 ;;;; Started: 04 Nov 2025 at 21:29
-;;;; Finished:
+;;;; Finished: 7 Nov 2025 at 10:48
 
 ;; ----------------------------------------------------------------------------
 ;; Prologue code for setup
@@ -31,8 +31,8 @@
   "Downloaded problem set")
 
 #| ----------------------------------------------------------------------------
-Quest 2: From Complex to Clarity
---- Part One ---
+                   Quest 2: From Complex to Clarity
+                           --- Part One ---
 
 LEO'S NOTES: Why this is just simple arithmetic. I'll create
 overloaded arithmetic functions for each of the "complex" operations
@@ -63,8 +63,9 @@ Even so-called trivial functions should have tests.
 
 (sr:-> parse-input (string) list)
 (defun parse-input (input)
-  "takes a string in the form A=[25,9] and returns a list of two integers"
-  (mapcar #'parse-integer (re:all-matches-as-strings "\\d+" input)))
+  "takes a string in the form A=[25,9] and returns a list of two integers
+- accomodates negative numbers with -?"
+  (mapcar #'parse-integer (re:all-matches-as-strings "-?[0-9]+" input)))
 
 (sr:-> complex-add (list list) list)
 (defun complex-add (pair1 pair2)
@@ -127,32 +128,77 @@ tranformation prescribed in the problem set to produce a result"
   (5a:is (equalp "[357,862]" (quest02-1 *example1*))))
 
 #| ----------------------------------------------------------------------------
---- Part Two ---
+                           --- Part Two ---
+
+LEO'S NOTES: I should be able to use the COMPLEX arithmetic functions
+from part 1. But there are quite a few more steps in the process. In
+fact, this part seems mainly to be a test to see how well I can read
+instructions.
+
+The final result is the number of points to be engraved. (I'll make a
+list of the actual engravable points in case I need it for part
+3. Also it might be cool to actually draw the design.)
 
 ---------------------------------------------------------------------------- |#
 
-(defparameter *example2* nil)
+(defparameter *example2* "A=[35300,-64910]")
 
-(defun quest02-2 (input)
-  nil
-  )
+(sr:-> engravable? (list) list)
+(defun engravable? (pt)
+  "given a point in a 2D space, PT, return PT if it's engravable,
+nil if not"
+  (let ((res '(0 0)))
+    (dotimes (i 100 res) ; returns result if engravable
+      (setf res
+            (sr:~> res
+                   (complex-multiply _ _)
+                   (complex-divide _ '(100000 100000))
+                   (complex-add _ pt)))
+      ;; make sure it's in range
+      (when (or (not (< -1000000 (first res)  1000001))
+                (not (< -1000000 (second res) 1000001)))
+        (return-from engravable? nil))))) ; out of range so bail out
+
+(5a:test engravable?-test
+  (5a:is (equalp '(-2520 -5355) (engravable? '(35630 -64880))))
+  (5a:is (equalp '(5021 6454) (engravable? '(35630 -64870))))
+  (5a:is-false (engravable? '(35460 -64910)))
+  (5a:is-false (engravable? '(35470 -64910))))
+
+(sr:-> quest02-2 (string fixnum) fixnum)
+(defun quest02-2 (input res)
+  "given a top left corner of a grid, and the resolution required, return
+the number of engravable points"
+  (let* ((top-left (parse-input input)) ; provided top left corner
+         (bottom-right (complex-add top-left '(1000 1000)))) ; 1000x1000 pts
+
+    (length ;; for this part, we just need the number of points
+     ;; make a list of engravable points
+     (iter
+       (for y from (second top-left) to (second bottom-right) by res)
+       (appending
+        (iter
+          (for x from (first top-left) to (first bottom-right) by res)
+          (when (engravable? (list x y)) ; collect engravable points
+            (collect (list x y)))))))))
 
 (5a:test quest02-2-test
-  (5a:is (= nil (quest02-2 (*example2*)))))
+  (5a:is (= 4076 (quest02-2 *example2* 10))))
 
 #| ----------------------------------------------------------------------------
---- Part Three ---
+                          --- Part Three ---
+
+Leo's NOTES: Same problem, just a finer grid. So go 1 by 1 instead of
+10 by 10. I'll modify the function from part 2 to add a second
+parameter: resolution. It's gonna be a bit slower, though. (Not too
+bad - less than 3 seconds on my M2 Macbook Air.)
 
 ---------------------------------------------------------------------------- |#
 
-(defparameter *example3* nil)
+(defparameter *example3* "A=[35300,-64910]")
 
-(defun quest02-3 (input)
-  nil
-  )
-
-(5a:test quest02-3-test
-  (5a:is (= nil (quest02-3 (*example3*)))))
+(5a:test quest02-2-test
+  (5a:is (= 406954 (quest02-2 *example3* 1))))
 
 ;; ----------------------------------------------------------------------------
 
@@ -160,12 +206,37 @@ tranformation prescribed in the problem set to produce a result"
 (time (format t "The answer to EC Quest 2 Part 1 is ~a"
               (quest02-1 (read-file-into-string *data-file1*))))
 
-;; (time (format t "The answer to EC Quest 2 Part 2 is ~a"
-;;	      (quest02-2 (read-file-into-string *data-file2*))))
+(time (format t "The answer to EC Quest 2 Part 2 is ~a"
+	      (quest02-2 (read-file-into-string *data-file2*) 10)))
 
-;; (time (format t "The answer to EC Quest 2 Part 3 is ~a"
-;;	      (quest02-3 (read-file-into-string *data-file3*))))
+(time (format t "The answer to EC Quest 2 Part 3 is ~a"
+	      (quest02-2 (read-file-into-string *data-file3*) 1)))
 
 ;; ----------------------------------------------------------------------------
 ;; Timings with SBCL on Framework Desktop w/ AMD AI Max+ 395, 128GB RAM
 ;; ----------------------------------------------------------------------------
+
+;; The answer to EC Quest 2 Part 1 is [421180,913532]
+;; Evaluation took:
+;; 0.000 seconds of real time
+;; 0.000250 seconds of total run time (0.000107 user, 0.000143 system)
+;; 100.00% CPU
+;; 0 bytes consed
+
+;; The answer to EC Quest 2 Part 2 is 683
+;; Evaluation took:
+;; 0.025 seconds of real time
+;; 0.025850 seconds of total run time (0.025276 user, 0.000574 system)
+;; [ Real times consist of 0.001 seconds GC time, and 0.024 seconds non-GC time. ]
+;; [ Run times consist of 0.001 seconds GC time, and 0.025 seconds non-GC time. ]
+;; 104.00% CPU
+;; 47,822,272 bytes consed
+
+;; The answer to EC Quest 2 Part 3 is 65623
+;; Evaluation took:
+;; 2.401 seconds of real time
+;; 2.428095 seconds of total run time (2.380647 user, 0.047448 system)
+;; [ Real times consist of 0.125 seconds GC time, and 2.276 seconds non-GC time. ]
+;; [ Run times consist of 0.125 seconds GC time, and 2.304 seconds non-GC time. ]
+;; 101.12% CPU
+;; 4,705,041,184 bytes consed
